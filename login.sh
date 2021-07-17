f006d98efdc084ca3bf58d03d3951849bffd080d
Tim van Wijk <tim@timvw.co.uk>
2021-07-17 14:02:57 +0100

    update login script

--- login.sh
+++ login.sh
@@ -4,3 +4,6 @@ printf "\n"
 kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
 
 printf "\n\n"
+
+
+kubectl proxy &
